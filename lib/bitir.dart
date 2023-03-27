a0de9756ef2911f2b7cd14444ca6b6c27ac0796c
Sentius <sentius@Sentius-MacBook-Pro.local>
2023-03-27 04:18:27 +0300

    yeni bir text eklendi

--- lib/bitir.dart
+++ lib/bitir.dart
@@ -22,6 +22,7 @@ class _BitirState extends State<Bitir> {
             Text(data[1].toString()),
             Text(data[2].toString()),
             Text(data[3].toString()),
+            Text("Github değişiklik kontrolü için eklendi"),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/');
